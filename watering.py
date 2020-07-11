7ac1713b39b2b207fa0c565d97e06e76113eda5c
mriedesel@verovis.com <mriedesel@verovis.com>
2020-07-11 14:09:32 +0200

    adjusted durations

--- watering.py
+++ watering.py
@@ -14,8 +14,8 @@ GPIO.setmode(GPIO.BCM)
 ###########################################################
 
 l_Pods = ['Salat', 'Gurke', 'Moehren', 'Zucchini', 'Petersilie']
-l_GPIO = [18, 23, 20, 16, 21] #, 24] #, 23, 24, 25, 12, 16, 20, 21] # select all GPIOs to use
-l_watering_duration = [10, 15, 10, 10, 10] #, 8] #, 3, 3, 3, 3, 3, 3, 3]
+l_GPIO = [18, 23, 20, 16, 21] # select all GPIOs to use
+l_watering_duration = [7, 12, 7, 7, 7] # select watering durations in seconds
 
 sleep_time = 1
 
